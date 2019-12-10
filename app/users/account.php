50639dd8dbf75732dc9f2105ccb318817b373001
Thomas Sönnerstam <sonnerstam.thomas@hotmail.com>
2019-12-10 21:58:41 +0100

    Changed variable names

--- app/users/account.php
+++ app/users/account.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 require __DIR__ . '/../autoload.php';
 
 if (isset($_POST["oldpassword"], $_POST["password"], $_POST["passwordrepeat"])) {
-    $oldPw = password_hash($_POST["oldpassword"], PASSWORD_DEFAULT);
-    $newPw = password_hash($_POST["password"], PASSWORD_DEFAULT);
-    $newPwRepeat = password_hash($_POST["passwordrepeat"], PASSWORD_DEFAULT);
+    $oldPassword = password_hash($_POST["oldpassword"], PASSWORD_DEFAULT);
+    $newPassword = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    $newPasswordRepeat = password_hash($_POST["passwordrepeat"], PASSWORD_DEFAULT);
 
     $statement = $pdo->prepare("SELECT * FROM users WHERE id = :id");
     $statement->execute([
@@ -15,12 +15,12 @@ if (isset($_POST["oldpassword"], $_POST["password"], $_POST["passwordrepeat"])) 
     ]);
     $user = $statement->fetch(PDO::FETCH_ASSOC);
 
-    $oldPwInfo = $user["password"];
+    $oldPasswordInfo = $user["password"];
 
-    if ($oldPwInfo === $oldPw && $newPw === $newPwRepeat && $newPw !== $oldPw) {
+    if ($oldPasswordInfo === $oldPassword && $newPassword === $newPasswordRepeat && $newPassword !== $oldPassword) {
         $changeQuery = $pdo->prepare("UPDATE users SET password = :newpassword WHERE id = :id");
         $changeQuery->execute([
-            ":newpassword" => $newPw,
+            ":newpassword" => $newPassword,
             ":id" => $_SESSION["user"]["id"]
         ]);
 
